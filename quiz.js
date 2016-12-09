3d77ac5c2bc51477f226de222c43d2871d76c3a5
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-09 15:15:27 -0600

    added eventlistener to each div

--- quiz.js
+++ quiz.js
@@ -1,7 +1,6 @@
 //declare glabal variable to store inventory of cars
-var wrapper = document.getElementById("wrapper");
 var globalCars;
-
+var wrapper = document.getElementById("wrapper");
 //request data from inventory json file
 var carRequest = new XMLHttpRequest();
 carRequest.addEventListener("load", displayCars);
@@ -9,21 +8,35 @@ carRequest.open("GET", "inventory.json");
 carRequest.send();
 
 
+var parseDisplay = "";
 function displayCars(e) {
     //parses data from request and stores it in global car variable
     globalCars = JSON.parse(e.target.responseText);
 
-    var parseDisplay = "";
     //loops through the array of objects
     for (var i = 0; i < globalCars.cars.length; i++) {
         console.log(globalCars.cars[i].make);
-        parseDisplay += `<div class="col-sm-3 indvDsp">
+        parseDisplay += `<div class="col-md-3 indvDsp" id="${[i]}">
                             <h3>${globalCars.cars[i].make} ${globalCars.cars[i].model}</h3>
                             <p>Year: ${globalCars.cars[i].year}</p>
                             <p>Price: ${globalCars.cars[i].price}</p>
                             <p>Desription ${globalCars.cars[i].description}</p>
                         </div>`
     }
+
     //sets html values to corresponding parsed data by indexing them via counter
-    wrapper.innerHTML = (parseDisplay);
+    wrapper.innerHTML = parseDisplay;
+
+    var idCounter = document.getElementsByClassName("indvDsp");
+    for (var i = 0; i < idCounter.length; i++) {
+        idCounter[i].addEventListener("click", function(e) {
+            e.currentTarget.style.background = "lightgrey";
+        })
+    }
+
 }
+
+
+//changes the background color of each item when clicked
+
+var displayBox = ("indvDsp");
