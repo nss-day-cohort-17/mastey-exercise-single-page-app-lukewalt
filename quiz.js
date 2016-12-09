b1a714a2614ab2eee99941fe3c67331d524e5447
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-09 16:12:55 -0600

    added method to clear the search field upon click of car element

--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
 //declare glabal variable to store inventory of cars
 var globalCars;
+var parseDisplay = "";
 var wrapper = document.getElementById("wrapper");
+
 //request data from inventory json file
 var carRequest = new XMLHttpRequest();
 carRequest.addEventListener("load", loadInventory);
@@ -14,9 +16,8 @@ function loadInventory(e) {
     populatePage();
 }
 
-var parseDisplay = "";
 function populatePage() {
-    //loops through the array of objects
+    //loops through the array of objects and compiles list of each car info
     for (var i = 0; i < globalCars.cars.length; i++) {
         console.log(globalCars.cars[i].make);
         parseDisplay += `<div class="col-md-3 indvDsp" id="${[i]}">
@@ -30,14 +31,14 @@ function populatePage() {
     //sets html values to corresponding parsed data by indexing them via counter
     wrapper.innerHTML = parseDisplay;
 
-    var idCounter = document.getElementsByClassName("indvDsp");
+    var indvDsp = document.getElementsByClassName("indvDsp");
     var searchInput = document.getElementById("inputField");
 
-    for (var i = 0; i < idCounter.length; i++) {
-        idCounter[i].addEventListener("click", function(e) {
+    for (var i = 0; i < indvDsp.length; i++) {
+        indvDsp[i].addEventListener("click", function(e) {
             e.currentTarget.style.background = "lightgrey";
             e.currentTarget.style.border = "5px solid black";
-            searchInput.innerHTML = "";
+            searchInput.value = "";
             searchInput.focus();
         })
     }
